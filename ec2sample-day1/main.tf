9cfee957ab55d6252997ff7ff33de1025118934b
meenigenikhitha <meenigenikhitha@gmail.com>
2025-05-02 12:19:00 +0530

    bnm

---
+++ ec2sample-day1/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "nikki" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = {
+      Name = "nikki"
+    }
+  
+}
---
+++ ec2sample-day1/variable.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    type = string
+    default = "value"
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = "value"
+  
+}
+
+variable "key_name" {
+    type = string
+    default = "value"
+  
+}
